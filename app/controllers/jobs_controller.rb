2f4abf14a4d5533fdc13d90cec43e34735a5b7fe
你的名字 <ksalsqzhbl@aliyun.com>
2017-01-09 19:36:56 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,6 +4,11 @@ before_filter :authenticate_user!, only: [:new, :create, :destroy, :update, :edi
 
 def show
   @job= Job.find(params[:id])
+
+  if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+      redirect_to root_path
+    end
 end
 
 def index
