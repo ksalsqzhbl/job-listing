c72886b09dcceae44ad750d1391063f58e49e0b6
你的名字 <ksalsqzhbl@aliyun.com>
2017-01-17 20:47:54 +0800

    change_welcome_index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :welcome do
+
+  end
 
   resources :jobs
-  root "jobs#index"
+  root 'welcome#index'
 
   resources :jobs do
       resources :resumes
